8b3e7392f8f9a125a8eeb1782e2c37b62b034429
Jojo123850 <giovanieandrianirina@gmail.com>
2026-07-29 14:37:14 +0200

    fix: burgerMenu

---
+++ src/components/BurgerMenu.tsx
@@ -0,0 +1,29 @@
+import { useState} from "react";
+import { Link } from "react-router-dom";
+
+
+
+// fonction pour le collapse BurgerMenu
+export default function BurgerM() {
+
+// variable par défaut false et fonction pour changer l'état du collapse
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+      <div>
+        <button id="burgerMenu" aria-expanded={isOpen} aria-label="Menu" onClick={() => setIsOpen(!isOpen)}>
+            <i className={`fa-solid ${isOpen ? "fa-xmark" : "fa-bars"}`}></i>
+        </button>
+        <div className="collapsediv">
+                <nav aria-expanded={isOpen}>
+                    <ul className={isOpen ? "menu-open" : "menu-closed"}>
+                        <li><Link to="/">Accueil</Link></li>
+                        <li><Link to="/APropos">A propos</Link></li>
+                        <li><Link to="/Contact">Contact</Link></li>
+                    </ul>
+                </nav>
+        </div>
+      </div>
+    
+  );
+}
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,22 +1,18 @@
 import { Link } from "react-router-dom";
 import "../styles/Navbar.scss";
+import BurgerM from "./BurgerMenu"; 
 
 // pour le header
 export default function Navbar(){
     return(
         <header>
-        <div>
+        <div className="headerDiv">
             <Link className="logo" to="/">
                 Kasa
             </Link>
-            <nav>
-                <ul>
-                    <li><Link to="/">Accueil</Link></li>
-                    <li><Link to="/APropos">A propos</Link></li>
-                    <li><Link to="/Contact">Contact</Link></li>
-                </ul>
-            </nav>
-            <i id="burgerMenu" className="fa-solid fa-bars"></i>
+
+            <BurgerM/>
+          
         </div>
     </header>
     )
